e45f52dfbbda7c1b1ba4cd03c35bb8787d1206f9
Cloud User <fuser@workstation-build.av43fzx3xlfexfldgd5nrc3tch.bx.internal.cloudapp.net>
2020-04-07 11:23:51 +0000

    initial commit

---
+++ server.js
@@ -0,0 +1,9 @@
+var http = require('http');
+var hostname = process.env.HOSTNAME || "Unknown"
+var handleRequest = function(request, response) {
+  console.log('Received request for URL: ' + request.url);
+  response.writeHead(200);
+  response.end('Hi there! I\'m being served from ' + hostname);
+};
+var www = http.createServer(handleRequest);
+www.listen(8080);
