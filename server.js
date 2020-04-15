d898f5cb58119597ff420270a19e7ead20353b17
Cloud User <fuser@workstation-build.av43fzx3xlfexfldgd5nrc3tch.bx.internal.cloudapp.net>
2020-04-15 10:57:16 +0000

    demo

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var hostname = process.env.HOSTNAME || "Unknown"
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello world! I\'m being served from ' + hostname);
+  response.end('Hi! I\'m being served from ' + hostname);
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
